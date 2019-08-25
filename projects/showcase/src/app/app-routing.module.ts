93d1e5555d72eafeb69ad068269d404b8648ce3b
florian-kittel <mail@florian-kittel.de>
2019-08-25 13:56:35 +0200

    feat(showcase): add base setup

--- projects/showcase/src/app/app-routing.module.ts
+++ projects/showcase/src/app/app-routing.module.ts
@@ -2,7 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(mod => mod.HomeModule)
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'input',
+    loadChildren: () => import('./input-page/input-page.module').then(mod => mod.InputPageModule)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/showcase/src/app/app.module.ts
+++ projects/showcase/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
