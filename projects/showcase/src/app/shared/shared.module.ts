74fde2e804afe9636960590d0d23c5f6ff882d8b
florian-kittel <mail@florian-kittel.de>
2019-08-25 13:54:49 +0200

    feat(showcase): add library link

---
+++ projects/showcase/src/app/shared/shared.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EssentialsModule } from '../../libs/essentials';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    EssentialsModule
+  ]
+})
+export class SharedModule { }
---
+++ projects/showcase/src/libs/essentials/index.ts
@@ -0,0 +1,1 @@
+export * from '../../../../essentials/src/public-api';
