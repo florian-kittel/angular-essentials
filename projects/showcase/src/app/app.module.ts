08c651d96ff582e44630974f137fdc7af0206e8b
florian-kittel <mail@florian-kittel.de>
2019-10-05 22:47:58 +0200

    feat(app): update app shell with navigation on left side

--- projects/showcase/src/app/app.module.ts
+++ projects/showcase/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    RouterModule
+    RouterModule,
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- projects/showcase/src/app/shared/shared.module.ts
+++ projects/showcase/src/app/shared/shared.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EssentialsModule } from '../../libs/essentials';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
   exports: [
     CommonModule,
+    ReactiveFormsModule,
     EssentialsModule,
   ]
 })
